10d2da218348f943f9fd9bda1427de7742406824
Natalia <natalia_alexandra@web.de>
2021-02-23 11:02:28 +0100

    templates

--- dev_summary/settings.py
+++ dev_summary/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dev_summary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
